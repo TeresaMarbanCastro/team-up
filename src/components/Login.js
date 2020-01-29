1697c977a28d9c68606fc2ededefd707426d5d97
Teresa <teresamarbancastro@gmail.com>
2020-01-29 14:47:31 +0100

    insert inputs

---
+++ src/components/Login.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default () => {
+    return (
+        <>
+        <input placeholder='user' type="text"></input>
+        <input placeholder='pass' type="password"></input>
+        </>
+    )}
